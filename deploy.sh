fa641a8ddfa3b4637e3495621a952a8b4c07f7bf
蒋福来 <jiangfulai@chexiao.co>
2022-10-11 17:45:25 +0800

    测试提交

--- deploy.sh
+++ deploy.sh
@@ -10,17 +10,15 @@ npm run build
 cd dist
 
 # 如果你要部署到自定义域名
-echo 'jiangfulai.com' > CNAME
+# echo 'jiangfulai.com' > CNAME
 
-#git init
+git init
 git checkout -b main
 git add -A
 git commit -m 'deploy'
 
-git push -f origin main
-
 # 如果你要部署在 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+git push -f https://github.com/const-JFL/pages-test.github.io.git main
 
 # 如果你要部署在 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
